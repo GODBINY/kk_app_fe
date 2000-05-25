cb325efac3e6169a076418f5e1beb7555c6397a8
godbin <arashi0121@naver.com>
2000-05-25 23:30:22 +0000

    feat: 우리만난지.. 부분 클래스 추가/ 이미지 추가

--- src/components/Days.jsx
+++ src/components/Days.jsx
@@ -6,10 +6,14 @@ const Days = () => {
     let dDay = calculateDday();
     return (
         <div className="days-content">
-            <div>뀽이랑 <GoHeartFill />콩이랑 만난지 {dDay}일이 되었어요!</div>
-            <div>우리뀽이 내가 인스타 글 남겨두었으니 꼭 봐요 힝</div>
-            <div>너무 많이많이 사랑하구</div>
-            <div>얼결해서 부부의날 실제로 꼭 챙기자?</div>
+            <div className="days-main-area">
+                <div>뀽이랑 <GoHeartFill />콩이랑 만난지 {dDay}일이 되었어요!</div>
+                <div>우리뀽이 내가 인스타 글 남겨두었으니 꼭 봐요 힝</div>
+                <div>너무 많이많이 사랑하구</div>
+                <div>얼결해서 부부의날 실제로 꼭 챙기자?</div>
+            </div>
+            <div className="days-sub-area">
+            </div>
         </div>
     );
 };
