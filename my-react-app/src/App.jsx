3d705d0f22e47d27b58d1e15c87feb9919e00483
godbin <arashi0121@naver.com>
2024-04-14 23:46:26 +0900

    login from init commint

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,45 @@
-// import ParentComponent from "./ParentComponent.jsx";
-// import TableComponent from "./TableComponent.jsx";
-import UserLoginComponent from "./UserLoginComponent.jsx"
-
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Login from './components/login.component.jsx'
+import SignUp from './components/signup.component.jsx'
 function App() {
-  return (
-      <>
-          <UserLoginComponent />
-        {/*<h1>Hello world!</h1>*/}
-        {/*<h2>my first react project!</h2>*/}
-        {/*<h2>comment inside return statement </h2>*/}
-        {/* curly brackets, forward slash, and asterisk format*/}
-        {/*  <ParentComponent />*/}
-        {/*  <TableComponent />*/}
-      </>
-  )
+    return (
+        <Router>
+            <div className="App">
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+                    <div className="container">
+                        <Link className="navbar-brand" to={'/sign-in'}>
+                            positronX
+                        </Link>
+                        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <Link className="nav-link" to={'/sign-in'}>
+                                        Login
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to={'/sign-up'}>
+                                        Sign up
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div className="auth-wrapper">
+                    <div className="auth-inner">
+                        <Routes>
+                            <Route exact path="/" element={<Login />} />
+                            <Route path="/sign-in" element={<Login />} />
+                            <Route path="/sign-up" element={<SignUp />} />
+                        </Routes>
+                    </div>
+                </div>
+            </div>
+        </Router>
+    )
 }
-
 export default App
-// normal comment
--- my-react-app/src/UserLoginComponent.jsx
+++ my-react-app/src/UserLoginComponent.jsx
@@ -1,36 +1,11 @@
-// import * as React from 'react';
-import Button from '@mui/material/Button';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
-export default function UserLoginComponent () {
-    const userId = ''
-    const password = ''
+function App() {
     return (
-        <>
-            <div>
-                ID : <input id={userId}></input>
-                PW : <input id={password}></input>
-                <ButtonContent contentYn="Y"/>
-                <Button variant="contained" onClick={showMessage}>Login</Button>
-            </div>
-        </>
+        <div className="App">
+            <h3>Build Sign Up& Login UI Template in React</h3>
+        </div>
     )
-}
-
-function ButtonContent(props) {
-    let button = <button>콘텐츠 없음</button>
-
-    if(props.contentYn) {
-        button = <button>콘텐츠 있음</button>
-    }
-
-    return(
-        <>
-            <h1>콘텐츠 유무 분기</h1>
-            {button}
-        </>
-    )
-}
-
-const showMessage = () => {
-    alert('로그인 했단다.')
 }
