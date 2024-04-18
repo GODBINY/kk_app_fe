d7a3e72696cf29885868f61fafb71e96f40a4f28
godbin <arashi0121@naver.com>
2024-04-18 23:46:09 +0900

    main frame

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,45 +1,25 @@
-import React from 'react'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import Login from './components/login.component.jsx'
-import SignUp from './components/signup.component.jsx'
-function App() {
-    return (
-        <Router>
-            <div className="App">
-                <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-                    <div className="container">
-                        <Link className="navbar-brand" to={'/sign-in'}>
-                            positronX
-                        </Link>
-                        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item">
-                                    <Link className="nav-link" to={'/sign-in'}>
-                                        Login
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to={'/sign-up'}>
-                                        Sign up
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <div className="auth-wrapper">
-                    <div className="auth-inner">
-                        <Routes>
-                            <Route exact path="/" element={<Login />} />
-                            <Route path="/sign-in" element={<Login />} />
-                            <Route path="/sign-up" element={<SignUp />} />
-                        </Routes>
-                    </div>
+import React from 'react';
+import { Card, Calendar } from 'antd-mobile';
+import {
+} from '@ant-design/icons';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                {/*<Card>*/}
+                {/*    <Card.Header title="뀽콩" />*/}
+                {/*    <Card.Body />*/}
+                {/*</Card>*/}
+                <div style={{ marginTop: '50px' }}>
+                    <h2>우리의 일정</h2>
+                    <Calendar />
+                    {/* 캘린더 컴포넌트를 넣어주세요 */}
                 </div>
             </div>
-        </Router>
-    )
+        );
+    }
 }
-export default App
+
+export default App;
