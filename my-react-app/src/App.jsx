5d49476083e90e962aeb542c31b2696dfc326b35
godbin <arashi0121@naver.com>
2024-05-01 22:02:00 +0900

    delete mainTemplate name

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import MainTemplate from './components/MainTemplate.jsx';
-import './App.css';
-import {
-} from '@ant-design/icons';
+import './App.scss';
 
 class App extends React.Component {
 
     render() {
-        return <MainTemplate className="root">뀽이와 콩이의 대시보드!</MainTemplate>
+        return <MainTemplate className="root" />
     }
 }
 
