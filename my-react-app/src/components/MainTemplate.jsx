9da456dba1d57c6a19d3d83b01be3a74f6008686
godbin <arashi0121@naver.com>
2024-05-03 23:35:07 +0900

    edit

--- my-react-app/src/components/MainTemplate.jsx
+++ my-react-app/src/components/MainTemplate.jsx
@@ -5,8 +5,25 @@ import { GoHeartFill } from "react-icons/go";
 const MainTemplate = () => {
     return (
         <div className="MainTemplate">
-            <div className="app-title">
-                <GoHeartFill /> 우리의 일정 <GoHeartFill />
+            <div className="d-day-title">
+                <div className="d-day-content">
+                    <img
+                        className="person-img"
+                         src="https://loremflickr.com/320/240?lock=1"
+                    />
+                    <div>뀽<GoHeartFill />콩</div>
+                    <div>우리 만난지</div>
+                    <div>D+502</div>
+                    <img
+                        className="person-img"
+                         src="https://loremflickr.com/320/240?lock=1"
+                    />
+                </div>
+            </div>
+            <div id="plans">
+                <div className="app-title">
+                    <GoHeartFill /> 우리의 일정 <GoHeartFill />
+                </div>
             </div>
             <Calendar />
         </div>
