cf80bb9eafe8580f50ecac1420544e41c0828cd8
godbin <arashi0121@naver.com>
2024-05-07 22:50:44 +0900

    feat: add setting button and apply z-index on calendar component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainTemplate from './components/main/MainTemplate.jsx';
+import './App.css';
+
+class App extends React.Component {
+
+    render() {
+        return <MainTemplate className="root" />
+    }
+}
+
+export default App;
