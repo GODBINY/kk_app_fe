a802dc64e67b98dda937cd6c31652c7a89514488
GODBIN <91115132+GODBINY@users.noreply.github.com>
2024-08-30 23:38:37 +0900

    Schedule.jsx 업데이트

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -22,7 +22,7 @@ const Home = () => {
         { placeCode: 2, placeName: '성남', startDate: '2024-07-12', endDate: '2024-07-14' },
         { placeCode: 1, placeName: '대전', startDate: '2024-07-19', endDate: '2024-07-21' },
         { placeCode: 2, placeName: '성남', startDate: '2024-07-26', endDate: '2024-07-28' },
-         { placeCode: 2, placeName: '성남', startDate: '2024-07-26', endDate: '2024-07-28' },
+        { placeCode: 2, placeName: '성남', startDate: '2024-07-26', endDate: '2024-07-28' },
     ];
 
     const formatDay = (locale, date) => date.getDate();
